f813e3cdc1c3747c2580bca5d5be41601c6f2fc6
Rinkle Seth <sethrinkle@gmail.com>
2018-11-01 19:44:16 -0500

    Update ClearDataInDb.sql

--- ClearDataInDb.sql
+++ ClearDataInDb.sql
@@ -1,4 +1,13 @@
+
 delete from url where id>285;
 delete from property where id > 0;
 delete from propertytype where id > 0;
+delete from expenses where id > 0;
+delete from expensetype where id > 0;
+delete from review where id > 0;
+delete from apartments where id > 0;
+delete from ntpi where id > 0;
+delete from school where id > 0;
+delete from amenity where id > 0;
+delete from amenitytype where id > 0;
 update url set status=0 where id = 285;
